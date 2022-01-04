ee61fff98c70a8f7ebb7fd39d4e178ec21cd07e1
dasari97 <dasarisaikrishna97@gmail.com>
2022-01-04 14:38:24 +0530

    .

--- userdata.sh
+++ userdata.sh
@@ -5,14 +5,16 @@ user=
 #Enter your password here
 password= 
 
+#creating files to redirect logs 
+touch /tmp/log
 ##adding user to instance owner 
-id owner &>>$LOG
+id $user &>>/tmp/log
 if [ $? -eq 0 ]; then
-    echo "User already there, So Skipping" &>>$LOG
+    echo "User already there, So Skipping" &>>/tmp/log
 else
     pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-    useradd "$user" -m -p "$pass" &>>$LOG
+    useradd "$user" -m -p "$pass" &>>/tmp/log
 fi
 
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/ig' /etc/ssh/sshd_config
-systemctl restart sshd  
+systemctl restart sshd
