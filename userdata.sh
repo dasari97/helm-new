7489328e7162205f696d0becf6842c6e574e9ed7
Dasari Sai Krishna <89766945+dasari97@users.noreply.github.com>
2022-01-11 13:43:55 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+##shell script or userdata for Linux instance to add a user.
 #Enter your username here
 name= 
 #Enter your password here
@@ -7,14 +7,19 @@ password=
 
 #creating files to redirect logs 
 touch /tmp/log
-##adding user to instance owner 
+##adding user to instance. 
 id "$name" &>>/tmp/log
 if [ $? -eq 0 ]; then
     echo "User already there, So Skipping" &>>/tmp/log
 else
     pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
     useradd "$name" -m -p "$pass" &>>/tmp/log
+    ##IF you want the user to be a sudoer, the uncomment this line
+    #sed -i -e "100 a $name  ALL=(ALL)       NOPASSWD: ALL" /etc/sudoers
+    
 fi
+#this command is used to enable password login into the Instance.
 
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/ig' /etc/ssh/sshd_config
 systemctl restart sshd
+##check the file /tmp/log for output details.
